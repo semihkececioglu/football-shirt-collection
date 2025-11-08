58d61b8f0fd4fe331694e2394e4d01cb309415de
semihkececioglu <semihkecec@gmail.com>
2025-11-09 00:49:11 +0300

    feat: add shirt service with CRUD operations and filter options

---
+++ client/src/services/shirtService.js
@@ -0,0 +1,63 @@
+import API from "./api";
+
+const shirtService = {
+  // Get all shirts with filters
+  getShirts: async (filters = {}) => {
+    const params = new URLSearchParams();
+
+    Object.keys(filters).forEach((key) => {
+      if (filters[key]) {
+        params.append(key, filters[key]);
+      }
+    });
+
+    const response = await API.get(`/shirts?${params.toString()}`);
+    return response.data;
+  },
+
+  // Get single shirt
+  getShirtById: async (id) => {
+    const response = await API.get(`/shirts/${id}`);
+    return response.data;
+  },
+
+  // Create new shirt
+  createShirt: async (formData) => {
+    const response = await API.post("/shirts", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return response.data;
+  },
+
+  // Update shirt
+  updateShirt: async (id, formData) => {
+    const response = await API.put(`/shirts/${id}`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return response.data;
+  },
+
+  // Delete shirt
+  deleteShirt: async (id) => {
+    const response = await API.delete(`/shirts/${id}`);
+    return response.data;
+  },
+
+  // Toggle favorite
+  toggleFavorite: async (id) => {
+    const response = await API.patch(`/shirts/${id}/favorite`);
+    return response.data;
+  },
+
+  // Get filter options
+  getFilterOptions: async () => {
+    const response = await API.get("/shirts/filters/options");
+    return response.data;
+  },
+};
+
+export default shirtService;
